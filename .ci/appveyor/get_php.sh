0783710494878e137fc7d31fb408898e136b78af
Nicholas Hutchinson <nshutchinson@gmail.com>
2018-09-04 22:29:27 +0100

    Initial commit

---
+++ .ci/appveyor/get_php.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+case "$OSTYPE" in
+    linux*)
+        sudo apt-get install -y --no-install-recommends \
+            php-cli \
+            php-curl \
+            php-mbstring \
+            php-xml
+        ;;
+
+    msys*)
+        curl -o php7.zip -fssL https://windows.php.net/downloads/releases/archives/php-7.2.9-nts-Win32-VC15-x64.zip || \
+            curl -o php7.zip -fssL https://windows.php.net/downloads/releases/php-7.2.9-nts-Win32-VC15-x64.zip
+        7z x php7.zip -ophp-7
+        sed -E \
+            -e 's/;extension=(curl|mbstring|openssl)/extension=\1/g' \
+            -e 's/; extension_dir = "ext"/extension_dir = "ext"/g' \
+            php-7/php.ini-production > php-7/php.ini
+        ;;
+esac
