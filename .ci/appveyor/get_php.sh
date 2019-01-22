59297ddea24f8c80d6eb8ec8c59813db25b95372
Nicholas Hutchinson <nshutchinson@gmail.com>
2019-01-22 19:44:20 +0000

    Update PHP version

--- .ci/appveyor/get_php.sh
+++ .ci/appveyor/get_php.sh
@@ -11,12 +11,12 @@ case "$OSTYPE" in
         ;;
 
     msys*)
-        curl -o php7.zip -fssL https://windows.php.net/downloads/releases/archives/php-7.2.9-nts-Win32-VC15-x64.zip || \
-            curl -o php7.zip -fssL https://windows.php.net/downloads/releases/php-7.2.9-nts-Win32-VC15-x64.zip
+        curl -o php7.zip -fssL https://windows.php.net/downloads/releases/archives/php-7.3.1-nts-Win32-VC15-x64.zip || \
+            curl -o php7.zip -fssL https://windows.php.net/downloads/releases/php-7.3.1-nts-Win32-VC15-x64.zip
         7z x php7.zip -ophp-7
         sed -E \
-            -e 's/;extension=(curl|mbstring|openssl)/extension=\1/g' \
-            -e 's/; extension_dir = "ext"/extension_dir = "ext"/g' \
+            -e 's/; *extension=(curl|mbstring|openssl)/extension=\1/g' \
+            -e 's/; *extension_dir = "ext"/extension_dir = "ext"/g' \
             php-7/php.ini-production > php-7/php.ini
         ;;
 esac
